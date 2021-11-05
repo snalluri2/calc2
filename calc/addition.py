eb018591af8abdfdcddceacc12283c4381259a96
snalluri2 <vk36@njit.edu>
2021-11-04 23:56:52 -0400

    task : cal part 2

---
+++ calc/addition.py
@@ -0,0 +1,8 @@
+"""This is addition operation object"""
+from calc.calculation import Calculation
+
+class Addition(Calculation):
+    """This is addition class"""
+    def get_result(self):
+        """This is addition class"""
+        return self.value_a + self.value_b
