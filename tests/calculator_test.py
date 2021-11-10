45e97c2ccd79d5c2206b41c7212e8e6745f01f07
snalluri2 <vk36@njit.edu>
2021-11-09 21:22:43 -0500

    Task : Refactoring the Python Calculator

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,43 +1,18 @@
 """Testing the Calculator"""
 import pytest
-from calculator.main import Calculator
+from calc.calculator import Calculator
+from calc.history.calculations import Calculations
+@pytest.fixture
+def clear_history_fixture():
+    Calculations.clear_history()
+def test_calculator_add_static(clear_history_fixture):
+    assert Calculator.add_numbers(1.0,2.0) == 3.0
 
-def test_calculator_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
+def test_calculator_subtract_static(clear_history_fixture):
+    assert Calculator.subtract_numbers(1.0,2.0) == -3.0
 
-def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(4)
-    #Assert that the results are correct
-    assert calc.result == 4
+def test_calculator_multiply_static(clear_history_fixture):
+    assert Calculator.multiply_numbers(1.0,2.0) == 2.0
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
-
-def test_calculator_subtract():
-    """Testing the subtract method of the calculator"""
-    calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
-def test_calculator_multiply():
-    """ tests multiplication of two numbers"""
-    calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
-    assert result == 2
-def test_calculator_division():
-    """ division two numbers and store the result"""
-    calc = Calculator()
-    result = calc.division_numbers(1,1)
-    assert result == 1
-def test_calculator_division_exception():
-    """ division exception """
-    calc = Calculator()
-    with pytest.raises(ZeroDivisionError):
-        calc.division_numbers(1,0)
+def test_calculator_divide_static(clear_history_fixture):
+    assert Calculator.divide_numbers(1.0,2.0) == 0.5
